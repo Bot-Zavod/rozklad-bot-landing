9cdd717fa53432ba6366ff20d933b34ad0acc30e
Yevhen Khmelevskyi <khmellevskyi.ye23@gmail.com>
2021-03-07 21:25:03 +0200

    Frontend is done

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, redirect
+
+
+app = Flask(__name__)
+
+
+
+
+
+@app.route('/', methods=['GET', 'POST'])
+def main_page():
+
+	return render_template('index.html')
+
+
+
+
+if __name__ == '__main__':
+	app.run(debug=True, use_reloader=True) # host='0.0.0.0', port=80
+
+# debug=True, use_reloader=True,
+
