40271cd142aa90792a6758c7b5349eb3edd81482
Yevhen Khmelevskyi <khmellevskyi.ye23@gmail.com>
2021-03-08 18:28:15 +0200

    minor changes

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def main_page():
 
 
 if __name__ == '__main__':
-	app.run(debug=True, use_reloader=True) # host='0.0.0.0', port=80
+	app.run(host='0.0.0.0', port=80) # host='0.0.0.0', port=80
 
 # debug=True, use_reloader=True,
 
